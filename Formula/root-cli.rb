8b00d599fce7f930892867c808fe4138f3c842b2
Sergio Rovira <sgr0691@users.noreply.github.com>
2026-05-29 02:40:49 -0400

    Add x86_64 macOS support

--- Formula/root-cli.rb
+++ Formula/root-cli.rb
@@ -7,12 +7,15 @@ class RootCli < Formula
   if OS.mac? && Hardware::CPU.arm?
     url "https://github.com/sgr0691/Root/releases/download/v#{version}/root-aarch64-apple-darwin.tar.gz"
     sha256 "5922ea7f1bce1d60dba190fd23ddf0453d30b5d345bc43a9e4f0f78518a721b1"
-
-    def install
-      bin.install "root"
-    end
+  elsif OS.mac? && Hardware::CPU.intel?
+    url "https://github.com/sgr0691/Root/releases/download/v#{version}/root-x86_64-apple-darwin.tar.gz"
+    sha256 "a2640b7e5745599669ebb4b57b4fe66bf592272ce08ce9dcddbbfd02c3c1eadf"
   else
-    odie "Root is currently only available on Apple Silicon (aarch64) macOS"
+    odie "Root v0.1.0 only supports macOS (arm64 and x86_64). Linux support coming soon."
+  end
+
+  def install
+    bin.install "root"
   end
 
   test do
